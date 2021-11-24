93735efd3eeaee3a76354d99dbbf244acd713b0d
Mohamad Alief Rizky Ramadhan <aliefm144@gmail.com>
2021-11-24 16:12:19 +0700

    add navbar

--- index.php
+++ index.php
@@ -9,10 +9,17 @@ include "./controller/connection.php"
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="./assets/css/style.css">
     <title>Belajar Pemrograman Web</title>
 </head>
 <body>
-    <h2 class="text-center mt-4">Belajar Web</h2>
+    <nav class="navigation">
+			<ul>
+				<li class="navbar"><a href="index.php">Home</a></li>
+				<li class="navbar"><a href="proker.html">Jadwal Mata Kuliah</a></li>
+			</ul>
+		</nav>
+    <h2 class="text-center mt-4">Data Mahasiswa</h2>
     <div class="row">
         <div class="col-md-2"></div>
         <div class="col-md">
